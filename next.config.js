45654b91d73664aad68758242f9d20950feb8741
Jonathan <jonathanaboagye66@gmail.com>
2026-07-27 20:36:40 +0000

    Remove next-pwa to fix build - will re-add later

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
-})
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // any other config you had
 }
 
-module.exports = withPWA(nextConfig)
+module.exports = nextConfig
